218479e19f8090c3306a7231f8b043bf2731c666
junhyeok <toughhyeok96@gmail.com>
2023-09-13 23:02:32 +0900

    first commit

---
+++ src/main/java/com/hotamul/music/moodbe/MoodbeApplication.java
@@ -0,0 +1,13 @@
+package com.hotamul.music.moodbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotamul/music/moodbe/MoodbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotamul.music.moodbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
