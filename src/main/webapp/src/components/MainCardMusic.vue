a3bffd7baa3290870bfb0afffb12d2a13dfc8e01
toughhyeok <toughhyeok96@gmail.com>
2023-09-19 21:46:46 +0900

    [refactoring] extract component nav, footer, card music

---
+++ src/main/webapp/src/components/MainCardMusic.vue
@@ -0,0 +1,122 @@
+<template>
+  <section class="py-5 bg-light">
+    <div class="container px-4 px-lg-5 mt-5">
+      <h2 class="fw-bolder mb-4">다른 추천 곡</h2>
+      <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+        <div class="col mb-5">
+          <div class="card h-100">
+            <!-- Badge-->
+            <div class="badge bg-success text-white position-absolute" style="top: 0.5rem; right: 0.5rem">New
+            </div>
+            <!-- Music image-->
+            <img class="card-img-top" src="../static/image/adoy_blanc.jpeg" alt="..." />
+            <!-- Music details-->
+            <div class="card-body p-4">
+              <div class="text-center">
+                <!-- Music name-->
+                <h5 class="fw-bolder">ADOY - Blanc</h5>
+                <!-- Music price-->
+                (feat. George)
+              </div>
+            </div>
+            <!-- Music actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+              <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-5">
+          <div class="card h-100">
+            <!-- Sale badge-->
+            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale
+            </div>
+            <!-- Music image-->
+            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+            <!-- Music details-->
+            <div class="card-body p-4">
+              <div class="text-center">
+                <!-- Music name-->
+                <h5 class="fw-bolder">Special Item</h5>
+                <!-- Music reviews-->
+                <div class="d-flex justify-content-center small text-warning mb-2">
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                </div>
+                <!-- Music price-->
+                <span class="text-muted text-decoration-line-through">$20.00</span>
+                $18.00
+              </div>
+            </div>
+            <!-- Music actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+              <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-5">
+          <div class="card h-100">
+            <!-- Sale badge-->
+            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale
+            </div>
+            <!-- Music image-->
+            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+            <!-- Music details-->
+            <div class="card-body p-4">
+              <div class="text-center">
+                <!-- Music name-->
+                <h5 class="fw-bolder">Sale Item</h5>
+                <!-- Music price-->
+                <span class="text-muted text-decoration-line-through">$50.00</span>
+                $25.00
+              </div>
+            </div>
+            <!-- Music actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+              <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-5">
+          <div class="card h-100">
+            <!-- Music image-->
+            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+            <!-- Music details-->
+            <div class="card-body p-4">
+              <div class="text-center">
+                <!-- Music name-->
+                <h5 class="fw-bolder">Popular Item</h5>
+                <!-- Music reviews-->
+                <div class="d-flex justify-content-center small text-warning mb-2">
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                </div>
+                <!-- Music price-->
+                $40.00
+              </div>
+            </div>
+            <!-- Music actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+              <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MainCardMusic',
+  props: {
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/main/webapp/src/components/MainMusic.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+
+    <!-- Navigation-->
+    <MainNavBar />
+    <!-- Product section-->
+    <section class="py-5">
+      <div class="container px-4 px-lg-5 my-5">
+        <div class="row gx-4 gx-lg-5 align-items-center">
+          <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="../static/image/adoy_blanc.jpeg"
+              alt="adoy_blanc" /></div>
+          <div class="col-md-6">
+            <div class="small mb-1">2023.09.19</div>
+            <h1 class="display-5 fw-bolder">ADOY - Blanc</h1>
+            <div class="fs-5 mb-5">
+              <span>(feat. George)</span>
+            </div>
+            <p class="lead">
+              오늘의 노래는 "ADOY"의 "Blanc"에요.
+              "You make me drop this way"라는 가사처럼 "Blanc"의 고조되는 비트와 "George"의 목소리에 푹 빠져보는 건 어떨까요?
+            </p>
+            <div class="d-flex">
+              <button class="btn btn-outline-dark flex-shrink-0" type="button">
+                <i class="bi-youtube me-1"></i>
+                Youtube에서 음악 듣기
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Card Music section-->
+    <MainCardMusic />
+    <!-- Footer-->
+    <MainFooter />
+  </div>
+</template>
+
+<script>
+import MainNavBar from './MainNavBar.vue';
+import MainFooter from './MainFooter.vue';
+import MainCardMusic from './MainCardMusic.vue';
+
+export default {
+  name: 'MainMusic',
+  components: {
+    MainNavBar,
+    MainCardMusic,
+    MainFooter,
+  },
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
