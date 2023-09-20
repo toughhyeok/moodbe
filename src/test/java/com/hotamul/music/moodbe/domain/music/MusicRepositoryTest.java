a8900b4319d92c44ba43f980310afcbe0c630475
toughhyeok <toughhyeok96@gmail.com>
2023-09-21 00:36:22 +0900

    [fix] no endline + import optimize

--- src/test/java/com/hotamul/music/moodbe/domain/music/MusicRepositoryTest.java
+++ src/test/java/com/hotamul/music/moodbe/domain/music/MusicRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
