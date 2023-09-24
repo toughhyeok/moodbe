35c635be13606549ed6cf4dd97dffeecf38d9b1b
toughhyeok <toughhyeok96@gmail.com>
2023-09-24 15:42:53 +0900

    [feature] vue3 -> vue2, dynamic loaing latest music

--- src/main/webapp/src/components/MainMusic.vue
+++ src/main/webapp/src/components/MainMusic.vue
@@ -7,23 +7,22 @@
     <section class="py-5">
       <div class="container px-4 px-lg-5 my-5">
         <div class="row gx-4 gx-lg-5 align-items-center">
-          <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="../static/image/adoy_blanc.jpeg"
-              alt="adoy_blanc" /></div>
+          <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" :src=latestMusic.imagePath alt="adoy_blanc" />
+          </div>
           <div class="col-md-6">
-            <div class="small mb-1">2023.09.19</div>
-            <h1 class="display-5 fw-bolder">ADOY - Blanc</h1>
+            <div class="small mb-1">{{ latestMusic.createdDate }}</div>
+            <h1 class="display-5 fw-bolder">{{ latestMusic.author }} - {{ latestMusic.title }}</h1>
             <div class="fs-5 mb-5">
-              <span>(feat. George)</span>
+              <span>(feat. {{ latestMusic.feat }})</span>
             </div>
-            <p class="lead">
-              오늘의 노래는 "ADOY"의 "Blanc"에요.
-              "You make me drop this way"라는 가사처럼 "Blanc"의 고조되는 비트와 "George"의 목소리에 푹 빠져보는 건 어떨까요?
-            </p>
+            <p class="lead">{{ latestMusic.content }}</p>
             <div class="d-flex">
-              <button class="btn btn-outline-dark flex-shrink-0" type="button">
-                <i class="bi-youtube me-1"></i>
-                Youtube에서 음악 듣기
-              </button>
+              <a :href="latestMusic.youtubeUrl" target="_blank"><button class="btn btn-outline-dark flex-shrink-0"
+                  type="button">
+                  <i class="bi-youtube me-1"></i>
+                  Youtube에서 음악 듣기
+                </button>
+              </a>
             </div>
           </div>
         </div>
@@ -40,6 +39,7 @@
 import MainNavBar from './MainNavBar.vue';
 import MainFooter from './MainFooter.vue';
 import MainCardMusic from './MainCardMusic.vue';
+import axios from 'axios';
 
 export default {
   name: 'MainMusic',
@@ -49,6 +49,25 @@ export default {
     MainFooter,
   },
   props: {
+  },
+  data: function () {
+    return {
+      latestMusic: {
+        createdDate: '',
+        title: '',
+        content: '',
+        author: '',
+        feat: '',
+        imagePath: '',
+        youtubeUrl: ''
+      }
+    }
+  },
+  mounted() {
+    axios.get('/music/latest')
+      .then(res => {
+        this.latestMusic = res.data;
+      })
   }
 }
 </script>
--- src/main/webapp/vue.config.js
+++ src/main/webapp/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: '../resources/static'
+  outputDir: '../resources/static',
+  devServer: {
+    proxy: 'http://localhost:8080'
+  }
 })
