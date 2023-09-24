3738e99877293bf253d36eb5ece8ce3fe879cb19
toughhyeok <toughhyeok96@gmail.com>
2023-09-24 19:30:12 +0900

    [feature] v-if rendering latest music

--- src/main/webapp/src/components/MainMusic.vue
+++ src/main/webapp/src/components/MainMusic.vue
@@ -4,15 +4,15 @@
     <!-- Navigation-->
     <MainNavBar />
     <!-- Product section-->
-    <section class="py-5">
+    <section class="py-5" v-if="latestMusic.id">
       <div class="container px-4 px-lg-5 my-5">
         <div class="row gx-4 gx-lg-5 align-items-center">
-          <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" :src=latestMusic.imagePath alt="adoy_blanc" />
+          <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" :src=latestMusic.imagePath />
           </div>
           <div class="col-md-6">
             <div class="small mb-1">{{ latestMusic.createdDate }}</div>
             <h1 class="display-5 fw-bolder">{{ latestMusic.author }} - {{ latestMusic.title }}</h1>
-            <div class="fs-5 mb-5">
+            <div class="fs-5 mb-5" v-if="latestMusic.feat">
               <span>(feat. {{ latestMusic.feat }})</span>
             </div>
             <p class="lead">{{ latestMusic.content }}</p>
@@ -53,6 +53,7 @@ export default {
   data: function () {
     return {
       latestMusic: {
+        id: null,
         createdDate: '',
         title: '',
         content: '',
@@ -73,4 +74,8 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+[v-cloak] {
+  display: none;
+}
+</style>
