dfc5b9aeff8be11c708b69afe447ea7d0bf1995d
toughhyeok <toughhyeok96@gmail.com>
2023-09-29 13:06:26 +0900

    [feature] music card hover shadow

--- src/main/webapp/src/components/MainCardMusic.vue
+++ src/main/webapp/src/components/MainCardMusic.vue
@@ -5,14 +5,13 @@
       <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
         v-if="allMusic.length !== 0">
         <div class="col mb-5" v-for="(music, index) in allMusic" v-bind:key="music.id">
-          <div class="card h-100">
+          <div class="card h-100 music-card" @click="clickMusicCard(music)">
             <!-- Badge-->
             <div v-if="index === 0" class="badge bg-success text-white position-absolute"
               style="top: 0.5rem; right: 0.5rem">New
             </div>
             <!-- Music image-->
-            <img class="card-img-top" style="cursor: pointer;" @click="clickMusicCard(music)" :src="music.imagePath"
-              :alt="music.author + ' - ' + music.title" />
+            <img class="card-img-top" :src="music.imagePath" :alt="music.author + ' - ' + music.title" />
             <!-- Music details-->
             <div class="card-body p-4">
               <div class="text-center">
@@ -53,4 +52,9 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.music-card:hover {
+  box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.175) !important;
+  cursor: pointer;
+}
+</style>
