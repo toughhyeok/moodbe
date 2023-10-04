f57b632a6f1171a5f6566614e4d47c24766491e0
toughhyeok <toughhyeok96@gmail.com>
2023-10-04 20:43:13 +0900

    [feature] add favicon, main logo

--- src/main/webapp/src/components/MainNavBar.vue
+++ src/main/webapp/src/components/MainNavBar.vue
@@ -1,13 +1,13 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container px-4 px-lg-5">
-      <a class="navbar-brand" href="#!">Moodbe</a>
+      <a class="navbar-brand" href="/"><img src="../assets/moodbe-logo.png" alt="moodbe-logo">&nbsp;Moodbe</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
           class="navbar-toggler-icon"></span></button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-          <li class="nav-item"><a class="nav-link inactive" href="#!">About</a></li>
+          <li class="nav-item"><div class="nav-link inactive">About</div></li>
           <li class="nav-item active"><a class="nav-link active" style="cursor: pointer;"
               @click="onClickAllMusic">Music</a></li>
         </ul>
